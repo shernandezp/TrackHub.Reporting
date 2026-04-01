f2f689d344656fefe7acdfda07747025c08c6663
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2026-03-31 21:40:16 -0400

    Add Geofence Events Report

--- src/Domain/Interfaces/Geofence/IGeofenceReader.cs
+++ src/Domain/Interfaces/Geofence/IGeofenceReader.cs
@@ -1,6 +1,9 @@
-﻿namespace TrackHub.Reporting.Domain.Interfaces.Geofence;
+﻿using TrackHub.Reporting.Domain.Records;
+
+namespace TrackHub.Reporting.Domain.Interfaces.Geofence;
 
 public interface IGeofenceReader
 {
     Task<IEnumerable<TransporterInGeofenceVm>> GetTransportersInGeofenceAsync(CancellationToken cancellationToken);
+    Task<IEnumerable<GeofenceEventReportVm>> GetGeofenceEventsAsync(FilterDto filters, CancellationToken cancellationToken);
 }
