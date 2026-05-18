1b99d8cb1556543248a5819dbc6200bbc64ddd47
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2026-05-18 18:28:43 -0400

    Add Serilog

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -19,6 +19,8 @@ using Common.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddTrackHubSerilog();
+
 var allowedCORSOrigins = builder.Configuration.GetSection("AllowedCorsOrigins").Get<string>();
 Guard.Against.Null(allowedCORSOrigins, message: $"Allowed Origins configuration for CORS not loaded");
 
